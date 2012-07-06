87aeaf8dd5ce08aee23f75323553e995f8a1a93b
reactormonk <reactormonk@72bbbca6-d526-0410-a7d9-f06f51895060>
2012-07-06 16:12:40 +0000

    test for generators
    git-svn-id: https://apertium.svn.sourceforge.net/svnroot/apertium/incubator/apertium-roh@39237 72bbbca6-d526-0410-a7d9-f06f51895060

---
+++ test_proc.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+# This script expects three files:
+# - dictionary (place path below)
+# - data files:
+#   - analyze
+#   - generate
+# The data files are as following:
+# input ; output \n
+# input ; output \n
+# ...
+#
+# run via ruby <this file>
+# or chmod +x <this file> and ./<this file>
+
+# path to dix here 
+DIX = 'generated.dix'
+
+require 'minitest/spec'
+require 'minitest/autorun'
+require 'shellwords'
+
+class LTProc
+  # this generates file.lr and file.rl in the directory
+  def initialize(dix, direction)
+    system ['lt-comp', direction, dix, dix + "." + direction].shelljoin
+    @proc = IO.popen(['lt-proc', '-z', dix + "." + direction], 'r+')
+    @proc.sync = true
+  end
+
+  def process(word)
+    @proc.write(word + " \x0")
+    @proc.flush
+    @proc.gets("\x0").strip
+  end
+end
+
+[['analyze', 'lr'], ['generate', 'rl']].each do |(type, direction)|
+  describe "#{type}r" do
+    if File.exists? type
+      proc = LTProc.new(DIX, direction)
+      # expectes either 'analyze' or 'generate'
+      File.read(type).each_line do |line|
+        input, output = *line.strip.split(";").map(&:strip)
+        it "should #{type} #{input}" do
+          proc.process(input).must_equal output
+        end if input
+      end
+    end
+  end
+end
